0b4b8e2a44041bb98fa641bf9930acf9ed046f10
ttak0422 <bgdaewalkman@gmail.com>
2019-03-14 01:59:38 +0900

    add 3_2_counter

---
+++ 3_2_counter/src/Main.fs
@@ -0,0 +1,40 @@
+module Counter
+
+
+open Elmish
+open Elmish.React
+open Fable.Helpers.React.Props
+module R =  Fable.Helpers.React
+
+
+// MODEL
+type Model = int
+
+let init () : Model =
+    0
+
+
+// UPDATE
+type Msg =
+    | Increment
+    | Decrement
+
+let update msg model =
+    match msg with
+    | Increment -> model + 1
+    | Decrement -> model - 1
+
+
+// VIEW
+let view model dispatch =
+    let onClick msg =
+        OnClick (fun _ -> dispatch msg)
+    R.div []
+        [ R.button [ onClick Decrement ] [ R.str "-"]
+          R.div [] [ R.str (string model) ]
+          R.button [ onClick Increment ] [ R.str "+"] ]
+
+
+Program.mkSimple init update view
+|> Program.withReact "elmish-app"
+|> Program.run
